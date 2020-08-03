007b206fda8d3f892e2480b72f11958b8c217abb
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-08-03 21:20:00 +0900

    doGet関数を作成

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,3 @@
-declare const global: {
-  [x: string]: any;
-};
-
-const greeter = (person: string) => {
-  return `Hello, ${person}!`;
-};
-
-// 関数一覧
-function testGreeter() {
-  const user = 'Ryusou';
-  Logger.log(greeter(user));
+function doGet() {
+  return HtmlService.createTemplateFromFile('index').evaluate();
 }
-
-// 関数を実行
-global.testGreeter = function() {
-  return testGreeter();
-};
