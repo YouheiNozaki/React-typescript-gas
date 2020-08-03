0318d4476d4b6ced831e520febb671e3cc5b02e6
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-08-03 22:43:09 +0900

    serverとclientのビルド設定

---
+++ src/client/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export function App() {
+  return (
+    <>
+      <p>Hello React/GoogleAppScripts！！！</p>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.client.config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/*    Google Apps Script Config
+================================== */
+
+const appsscriptConfig = {
+  entry: './appsscript.json',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: './appsscript.json' }],
+    }),
+  ],
+};
+
+const config = {
+  entry: './src/client/App.tsx',
+  output: {
+    path: resolve('dist'),
+    filename: 'App.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      inlineSource: '.(js|css¥tsx)$',
+      template: './public/index.html',
+    }),
+    new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
+  ],
+  externals: [
+    {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@material-ui/core': 'MaterialUI',
+      'react-router-dom': 'ReactRouterDOM',
+    },
+  ],
+};
+
+module.exports = [config, appsscriptConfig];
