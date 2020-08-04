c293ef2aef8118d90d10fb797ba00575bbf65bfd
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-08-04 22:48:47 +0900

    Home とAboutページのルーティングを作成

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { About } from './pages/About';
 
 export function App() {
   return (
-    <>
-      <p>Hello React/GoogleAppScripts！！！</p>
-    </>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </div>
   );
 }
 
